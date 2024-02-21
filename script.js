9a4a7d5ae2859dc894775aec81586194dc2282b5
code-with-ian-lgtm <nicholasian2304@gmail.com>
2024-02-21 17:09:46 +0300

    navlist update

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 120);
 })
+
+let menu = document.querySelector('menu-icon');
+let navList = document.querySelector('.navList');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navList.classList.toggle('active');
+
+}
